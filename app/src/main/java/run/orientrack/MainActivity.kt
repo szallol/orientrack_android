ff9323a2ffc4cd0d69056107d81298b3402b7e85
Szalló László <szallol@gmail.com>
2023-11-16 16:54:07 +0200

    -missing from last commit

--- app/src/main/java/run/orientrack/MainActivity.kt
+++ app/src/main/java/run/orientrack/MainActivity.kt
@@ -1,11 +1,31 @@
 package run.orientrack
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import com.ncorti.slidetoact.SlideToActView
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+            ),
+            0
+        )
         setContentView(R.layout.activity_main)
+
+        val sta = findViewById<SlideToActView>(R.id.start) as SlideToActView
+//        sta.onSlideCompleteListener = object : SlideToActView.OnSlideCompleteListener {
+//            override fun onSlideComplete(view: SlideToActView) {
+//                // Your own implementation
+//            }
+//        }
     }
 }
